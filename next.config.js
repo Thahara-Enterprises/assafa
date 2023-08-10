8b6e4a31b975cc53ff6f3dae503e2086bbc66742
sheikh-zafar <audios.shkzafar@gmail.com>
2023-08-10 20:12:46 +0400

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  output: "standalone",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
