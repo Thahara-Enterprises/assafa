824d7aa36fafdfa1b2164777d7de48119828407c
sheikh-zafar <audios.shkzafar@gmail.com>
2023-10-04 21:04:58 +0400

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,22 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/651d965deb150b3fb99e1e94/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+              `,
+          }}
+        ></script>
 
         <body>
           <Main />
